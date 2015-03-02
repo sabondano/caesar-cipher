0d7f4083a0a1b0a79089964879e0192e3d710a29
Sebastian Abondano <sabondano1@gmail.com>
2015-03-01 21:55:38 -0500

    first draft

---
+++ cipher.rb
@@ -0,0 +1,28 @@
+class Cipher
+  attr_accessor :message, :cipher
+
+  def initialize(message, factor)
+    @message = message
+    @factor = factor
+    @cipher = []
+  end
+
+  def caesar_cipher
+    @message.each_char do |char|
+      if " " == char
+        @cipher << char.ord
+      else
+        @cipher << char.ord + @factor
+      end
+    end
+
+    @cipher = @cipher.map do |modified_ordinal|
+      modified_ordinal.chr
+    end
+
+    @cipher.join
+  end
+end
+
+message_1 = Cipher.new("What a string!", 5)
+puts message_1.caesar_cipher
